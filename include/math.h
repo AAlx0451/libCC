41ef043d9eb16a2c33b3a09f4bae944ddcfb9c19
Polina <aalx2176@gmail.com>
2026-04-07 10:35:58 +0300

    feat: math.h interface

---
+++ include/math.h
@@ -0,0 +1,29 @@
+#ifndef MATH_H
+#define MATH_H
+
+#define HUGE_VAL  (1.0/0.0)
+
+double acos(double x);
+double asin(double x);
+double atan(double x);
+double atan2(double y, double x);
+double ceil(double x);
+double cos(double x);
+double exp(double x);
+double fabs(double x);
+double floor(double x);
+double fmod(double x, double y);
+double frexp(double x, int *exp);
+double ldexp(double x, int exp);
+double log(double x);
+double log10(double x);
+double modf(double x, double *iptr);
+double pow(double x, double y);
+double sin(double x);
+double sqrt(double x);
+double tan(double x);
+double sinh(double x);
+double cosh(double x);
+double tanh(double x);
+
+#endif /* !MATH_H */
